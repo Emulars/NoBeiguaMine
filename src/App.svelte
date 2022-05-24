8568c745eda1370562f0f1741a56c36464310f29
Elisa Calza <S4700104@studenti.unige.it>
2022-05-24 11:49:29 +0200

    Navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,58 @@
-<script>
-	export let name;
-</script>
+<div class="first_block">
+	<div class="logo" />
+	<div class="link">
+		<span><a href="bjwdhaj">HOME</a></span>
+		<span><a href="bjwdhaj">ARTICOLI</a></span>
+	</div>
+		
+</div>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="second_block">
+	<img src="" />
+</div>
 
+<div class="third_block">
+	<p>schede</p>
+</div>
+
+<!-- ---------------------------------STILE-------------------------------- -->
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap");
+
+	:global(body) {
+		padding: 0%;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.logo {
+		height: 80px;
+		width: 90px;
+		margin-left: 100px;
+		margin-top: 5px;
+		background-color: black;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.first_block {
+		display: flex;
+		justify-content: space-between;
+		height: 90px;
+		width: 100%;
+		background-color: rgb(255, 255, 255);
+		box-shadow: 3px 3px 3px rgb(164, 150, 150);
+		z-index: 1;
 	}
+
+	.link {
+		margin-top: 30px;
+		margin-right: 100px;
+		margin-left: 100px;
+		text-align: right;
+	}
+
+	a {
+		font-family: "Oswald Bold", sans-serif;
+		margin-right: 20px;
+		text-decoration: none;
+		color: black;
+	}
+	
 </style>
