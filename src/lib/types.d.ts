1157624a0192eedc4b4c16ebfd0691f0367a17a5
Emulars <davide.miggiano.majo@gmail.com>
2022-06-26 17:48:00 +0200

    Clean up

--- src/lib/types.d.ts
+++
@@ -1,9 +0,0 @@
-type TimelinePosition = 'right' | 'left' | 'alternate';
-
-type ParentPosition = 'right' | 'left';
-
-type TimelineConfig = {
-	rootPosition: TimelinePosition;
-};
-
-export { TimelinePosition, ParentPosition, TimelineConfig };
