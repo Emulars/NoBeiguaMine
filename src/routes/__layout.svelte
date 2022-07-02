9bd5f1bf9bcd012b3e028864ab1a8310f62a6307
Elisa Calza <S4700104@studenti.unige.it>
2022-07-02 19:39:00 +0200

    cookie(non funzia ancora)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,17 @@
-<script lang="ts">
+<script>
 	import NavBar from "$lib/components/NavBar.svelte";
 	import Footer from "$lib/components/footer.svelte";
 	import Cookies from "$lib/components/cookies.svelte";
+	
+	import '@beyonk/gdpr-cookie-consent-banner/dist/style.css'; // optional, you can also define your own styles
+  	import GdprBanner from '@beyonk/gdpr-cookie-consent-banner';
+
+  function initAnalytics () {
+    // do some stuff here
+  }
 </script>
 
-
+<div>
 <!-- NavBar  -->
 <NavBar/>
 
@@ -19,7 +26,11 @@
 <!-- Cookies  -->
 <Cookies />
 
-<style>
-
-
-</style>
+<GdprBanner description="Ben arrivato! Ti preghiamo di considerare di consentirci di avere il tuo consenso per memorizzare i cookie"
+			heading= 'Pagina dei Cookie'
+			acceptLabel= 'Accetta tutti i cookie'
+  			settingsLabel= 'Scegli i cookie'
+  			closeLabel= 'Chiudi'
+			showEditIcon= false
+   on:analytics={initAnalytics} />
+</div>
